1e19e9d53b594aafc6b49cffbc33206527d2942c
ks121212 <124970377+ks121212@users.noreply.github.com>
2023-03-13 12:25:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function foo(){
+  var k=document.getElementById("first").value;
+  console.log(k);
+  var k1=document.getElementById("middle").value;
+  console.log(k1);
+  var k2=document.getElementById("last").value;
+  console.log(k2);
+  var k3=document.getElementById("email").value;
+  console.log(k3);
+  var k4=document.getElementById("pass").value;
+  console.log(k4);
+}
+
